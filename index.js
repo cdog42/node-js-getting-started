25006630ae605987568b6f6aae2e05ec2f021fbf
“Craig <tcraigscott@gmail.com>
2024-06-13 15:05:47 -0700

    commit before adding mongo

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const path = require('path');
 const PORT = process.env.PORT || 5001;
 
 const app = express();
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
 
 app.use(express.static(path.join(__dirname, 'public')))
    .set('views', path.join(__dirname, 'views'))
@@ -16,8 +25,25 @@ app.use(express.static(path.join(__dirname, 'public')))
    .get('/', (req, res) => res.render('pages/index'))
    .get('/cool', (req, res) => res.send(cool()))
    .get('/times', (req, res) => res.send(showTimes()))
-   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+   .listen(PORT, () => console.log(`Listening on ${PORT}`))
+   .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+   
+   
+   ;
 
+
+   
 function showTimes() {
   const times = process.env.TIMES || 8;
   let result = '';
