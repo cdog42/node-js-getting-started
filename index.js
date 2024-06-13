7b559408178193551690254ce6b239070f7cb370
“Craig <tcraigscott@gmail.com>
2024-06-13 12:40:33 -0700

    added env stuff

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-
-const PORT = process.env.PORT || 5001
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 
 
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+   .set('views', path.join(__dirname, 'views'))
+   .set('view engine', 'ejs')
+   .get('/', (req, res) => res.render('pages/index'))
+   .get('/cool', (req, res) => res.send(cool()))
+   .get('/times', (req, res) => res.send(showTimes()))
+   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+function showTimes() {
+  const times = process.env.TIMES || 5;
+  let result = '';
+  for (let i = 0; i < times; i++) {
+    result += i + ' ';
+  }
+  return result;
+}
